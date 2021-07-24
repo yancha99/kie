0d9962e1331e2495e0a70f3e4867cf8051907b79
yancha99 <86874065+yancha99@users.noreply.github.com>
2021-07-24 18:09:46 +0700

    summary

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "gizi";
+
+// Koneksi dan memilih database di server
+$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+?>
